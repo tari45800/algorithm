ed4f763e52f9d2a874a095bb2fee8f12c2389b19
tari45800 <tari45800@gmail.com>
2023-01-27 00:34:07 +0900

    0127 오전커밋

---
+++ test/test.js
@@ -0,0 +1,14 @@
+function removeExtremes(arr) {
+  // TODO: 여기에 코드를 작성합니다.
+
+  let l = [arr[0].length, arr[0].length]
+  let na = [];
+
+  for(let i of arr){
+    if(!(i.length >= l[0].length || i.length <= l[1].length)){
+      
+    }
+  }
+
+  return l;
+}
